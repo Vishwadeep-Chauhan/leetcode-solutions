2d1ea179aebd69f6a9eeb7c86f53a7b57ed54ebd
Vishwadeep Chauhan <gc4840921@gmail.com>
2026-08-24 12:29:16 +0530

    Time: 21 ms (5.24%), Space: 47.1 MB (20.26%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+     HashMap<Character,Integer>map=new HashMap<>();   
+    
+    for(int i=0;i<magazine.length();i++){
+        char ch=magazine.charAt(i);
+        if(map.containsKey(ch)){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        else{
+            map.put(ch,1);
+        }
+    }
+    for(int i=0;i<ransomNote.length();i++){
+        char ch=ransomNote.charAt(i);
+        if(!map.containsKey(ch) || map.get(ch)<=0){
+            return false;
+        }
+        map.put(ch,map.get(ch)-1);
+    }
+    return true;
+}
+}
