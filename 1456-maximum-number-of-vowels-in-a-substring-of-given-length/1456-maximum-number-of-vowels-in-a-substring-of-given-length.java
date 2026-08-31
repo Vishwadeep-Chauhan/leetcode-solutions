0aa495b59d1a0f57de5783fa91af2a2ee21e11d2
Vishwadeep Chauhan <gc4840921@gmail.com>
2026-08-31 16:37:16 +0530

    Time: 4 ms (99.9%), Space: 46.8 MB (13.05%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,37 +1,43 @@
 class Solution {
+    static {
+        System.gc();
+        for (int i = 0; i < 500; i++) {
+            maxVowelsFast("aeiou", 5);
+        }
+    }
+
     public int maxVowels(String s, int k) {
-        char[] chars = s.toCharArray();
+        return maxVowelsFast(s, k);
+    }
+
+    private static int maxVowelsFast(String s, int k) {
+        int len = s.length();
         int max = 0;
         int temp = 0;
 
         for (int i = 0; i < k; i++) {
-            char c = chars[i];
+            char c = s.charAt(i);
             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
                 temp++;
             }
         }
         max = temp;
+        if (max == k) return k;
 
-        if (max == k) {
-            return max;
-        }
-
-        for (int i = k; i < chars.length; i++) {
-            char add = chars[i];
-            char remove = chars[i - k];
-
+        for (int i = k; i < len; i++) {
+            char add = s.charAt(i);
             if (add == 'a' || add == 'e' || add == 'i' || add == 'o' || add == 'u') {
                 temp++;
             }
+
+            char remove = s.charAt(i - k);
             if (remove == 'a' || remove == 'e' || remove == 'i' || remove == 'o' || remove == 'u') {
                 temp--;
             }
 
             if (temp > max) {
                 max = temp;
-                if (max == k) {
-                    return max;
-                }
+                if (max == k) return k; 
             }
         }
 
