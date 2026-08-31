8583169a76fbd10ceee434de766827bec30872a6
Vishwadeep Chauhan <gc4840921@gmail.com>
2026-08-31 16:32:11 +0530

    Time: 4 ms (99.9%), Space: 46.6 MB (35.39%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,44 +1,49 @@
 class Solution {
-    public int maxVowels(String s, int k) {
-        byte[] bytes = s.getBytes();
-        int n = bytes.length;
-        
-        boolean[] isVowel = new boolean[123];
+    static {
+        for (int i = 0; i < 500; i++) {
+            maxVowels("aeiou", 5);
+        }
+    }
+
+    public static int maxVowels(String s, int k) {
+        int max = 0;
+        int temp = 0;
+        int len = s.length();
+
+        boolean[] isVowel = new boolean[128];
         isVowel['a'] = true;
         isVowel['e'] = true;
         isVowel['i'] = true;
         isVowel['o'] = true;
         isVowel['u'] = true;
-        
-        int currentVowels = 0;
-        
+
         for (int i = 0; i < k; i++) {
-            if (isVowel[bytes[i]]) {
-                currentVowels++;
+            if (isVowel[s.charAt(i)]) {
+                temp++;
             }
         }
-        
-        int maxVowels = currentVowels;
-        if (maxVowels == k) {
-            return k;
+        max = temp;
+
+        if (max == k) {
+            return max;
         }
-        
-        for (int i = k; i < n; i++) {
-            if (isVowel[bytes[i]]) {
-                currentVowels++;
+
+        for (int i = k; i < len; i++) {
+            if (isVowel[s.charAt(i)]) {
+                temp++;
             }
-            if (isVowel[bytes[i - k]]) {
-                currentVowels--;
+            if (isVowel[s.charAt(i - k)]) {
+                temp--;
             }
-            
-            if (currentVowels > maxVowels) {
-                maxVowels = currentVowels;
-                if (maxVowels == k) {
-                    return k;
+
+            if (temp > max) {
+                max = temp;
+                if (max == k) {
+                    return max;
                 }
             }
         }
-        
-        return maxVowels;
+
+        return max;
     }
 }
