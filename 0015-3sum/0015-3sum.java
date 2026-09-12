021e009dfc89bf3069454fd5b2cf10abdb23bad9
Vishwadeep Chauhan <gc4840921@gmail.com>
2026-09-12 23:12:27 +0530

    Time: 36 ms (28.3%), Space: 59.2 MB (59.66%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,45 +1,52 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-public class Solution {
+class Solution {
+
     public List<List<Integer>> threeSum(int[] nums) {
+
         List<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
-        
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (nums[i] > 0) {
-                break;
-            }
-            if (i > 0 && nums[i] == nums[i - 1]) {
+
+        for(int i = 0; i < nums.length - 2; i++){
+
+            if(i > 0 && nums[i] == nums[i - 1]){
                 continue;
             }
-            
-            int left = i + 1;
-            int right = nums.length - 1;
-            
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
+
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            while(l < r){
+
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if(sum == 0){
+
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
+
+                    l++;
+                    r--;
+
+                    while(l < r && nums[l] == nums[l - 1]){
+                        l++;
                     }
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
+
+                    while(l < r && nums[r] == nums[r + 1]){
+                        r--;
                     }
-                    
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+                }
+
+                else if(sum < 0){
+                    l++;
+                }
+
+                else{
+                    r--;
                 }
             }
         }
+
         return result;
     }
 }
