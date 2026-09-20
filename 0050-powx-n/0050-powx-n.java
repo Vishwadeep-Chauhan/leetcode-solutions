35e89355c473914b919950cecc5b13de05b13c60
Vishwadeep Chauhan <gc4840921@gmail.com>
2026-09-20 22:33:35 +0530

    Time: 0 ms (100%), Space: 48 MB (45.97%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-    double y=Math.pow(x,n);
-    return y;
+        long power = n;
+
+        if(power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double result = 1;
+
+        while(power > 0) {
+            if(power % 2 == 1) {
+                result = result * x;
+            }
+
+            x = x * x;
+            power = power / 2;
+        }
+
+        return result;
     }
 }
